1f3e4ff7c5e0008574830675b4d09d81b1b902a3
johnkolesa03 <112509331+johnkolesa03@users.noreply.github.com>
2023-04-05 18:24:23 -0600

    11

---
+++ ica/ica11.js
@@ -0,0 +1,40 @@
+const button = document.querySelector("#js-new-quote");
+button.addEventListener('click', getQuote);
+
+
+
+
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+
+async function getQuote() {
+
+    try{
+        const response = await fetch(endpoint)
+        if (!response.ok){
+            throw Error(response.statusText)
+        }
+        const json = await response.json();
+
+        displayQuote(json.question);
+        showAnswer(json.answer);
+
+    }
+    catch (err){
+        console.log(err);
+        alert('Failed to fetch new Trivia');
+    }
+}
+
+function displayQuote(quote){
+    const quoteText = document.querySelector("#js-quote-text");
+    quoteText.textContent = quote;
+}
+
+function displayAnswer(quote){
+    const answerText = document.querySelector("#js-answer-text");
+    Text.textContent = quote;
+}
+
+
+getQuote();
